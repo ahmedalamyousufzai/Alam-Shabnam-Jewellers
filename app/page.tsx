94fc1cd31d42b91fc3e88ba99af74664c1347629
ahmedalamyousufzai <ahmedalamyousufzai@gmail.com>
2026-03-21 00:46:45 +0500

    Update homepage/banner responsive fix

--- app/page.tsx
+++ app/page.tsx
@@ -66,7 +66,7 @@ export default function Home() {
         <div className="relative z-10">
 
         {/* HERO SECTION */}
-        <section className="relative w-full min-h-[280px] md:min-h-[550px] flex items-center justify-center overflow-hidden">
+        <section className="relative w-full min-h-[240px] md:min-h-[550px] flex items-center justify-center overflow-hidden">
   <Image
     src="/images/banner.jpg"
     alt="Banner"
