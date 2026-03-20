8734b3e0c5a0cabdaec381809f8bdcbb15a4c095
ahmedalamyousufzai <ahmedalamyousufzai@gmail.com>
2026-03-21 00:19:17 +0500

    Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -12,11 +12,16 @@ export const metadata: Metadata = {
   description: "Designed by Avrx Tech",
 };
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased`}>
